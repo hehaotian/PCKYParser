e39509d820b8b7d2d5adb10ad909e883b061355c
Haotian He <haotianh@uw.edu>
2014-02-06 10:53:05 -0800

    init

---
+++ parse.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class parse {
+	public static void main(String[] args) throws IOException {
+		String pcfg_path = args[0];
+		String sent_path = args[1];
+		PrintStream parses = new PrintStream(args[2]);
+
+		PCKY parser = new PCKY(pcfg_path);
+
+		BufferedReader br = new BufferedReader(new FileReader(sent_path));
+		String sentLine = "";
+    	while ((sentLine = br.readLine()) != null) {
+    		String[] tokens = tokenizer(sentLine);
+			parser.best_parse();
+		}
+	}
+
+	private String[] tokenizer(String line) {
+		
+	}
+}
--- pcky.java
+++ pcky.java
@@ -1,13 +1,18 @@
 import java.io.*;
 import java.util.*;
+import java.math.*;
+import java.lang.*;
 
-public class pcky {
-	public static void main(String[] args) throws IOException {
-		String pcfg_path = args[0];
-		String sent_path = args[1];
-		PrintStream parses = new PrintStream(args[2]);
+public class PCFG {
+   
+   private static String cfg_path;
+   // key: lhs; value: list of rhs
+   private static Map<String, List<String>> rules = new HashMap<String, List<String>>();
+   // key: lhs; value: <rhs, rule_count>
+   private static Map<String, Integer> rules_count = new HashMap<String, Integer>();
 
-		BufferedReader sents = new BufferedReader(new FileReader(sent_path));
-
-	}
+   public PCFG(String cfg_path) throws IOException {
+      this.cfg_path = cfg_path; 
+      get_rules(); 
+   }
 }
